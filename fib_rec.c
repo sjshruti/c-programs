544debc2a22d9036dfd9b971a64e452ba61d99b9
sjshruti <108573266+sjshruti@users.noreply.github.com>
2022-08-28 16:45:30 +0545

    Create fib_rec.c

---
+++ fib_rec.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fib(int);
+int main()
+{
+  int n,i;
+  printf("enter the number:");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+    {
+      printf("%d\n",fib(i));
+      }
+  return 0;
+  }
+int fib (int n)
+{
+  if(n==0)
+    return 0;
+    else if(n==1)
+      return 1;
+  else
+    return(fib(n-1)+fib(n-2));
+}
